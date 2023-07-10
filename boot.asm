974ea68e9f6ebd3e59b8ba5e36e4f04b69dbbac1
Alexian Hentiu <alexianhentiu1208@gmail.com>
2023-07-11 02:10:51 +0300

    chapter 2

--- boot.asm
+++ boot.asm
@@ -1,3 +1,22 @@
-jmp $
-times 510-($-$$) db 0
-db 0x55, 0xAA
+    mov ah, 0xE     ; teletype mode
+    mov al, 'a'     ; char to print
+
+LOOP:
+    test al, 0x1    ; al % 2 == 0 ?
+    jz EVEN
+    sub al, 'a'-'A'     ; convert to uppercase
+    jmp PRINT
+
+EVEN:
+    add al, 'a'-'A'     ; convert to lowecase
+
+PRINT:
+    int 0x10    ; BIOS interrupt
+    inc al
+    cmp al, 'z' ; loop condition
+    jbe LOOP
+
+    jmp $   ; loop forever
+
+    times 510-($-$$) db 0   ; padding zeros
+    db 0x55, 0xAA           ; mark end of boot sector
